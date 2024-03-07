f0554b5964c696b654edbaaa83a68d8cf69f1328
SoponyaiAron <aron.soponyai@katonaj-mkszig.hu>
2024-03-07 10:12:50 +0100

    asd

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace WpfApp1
 {
@@ -23,14 +24,43 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
-        }
+            List<string> lines = ReadLinesFromFile("yourfile.txt");
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        }
+        private List<string> ReadLinesFromFile(string filePath)
+        {
+            List<string> lines = new List<string>();
+
+
+            try
+            {
+                // Read all lines from the file and add them to the list
+                string[] fileLines = File.ReadAllLines(filePath);
+                lines.AddRange(fileLines);
+            }
+            catch (IOException e)
+            {
+                MessageBox.Show($"Error reading file: {e.Message}", "File Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            return lines;
+        }
+private void Button_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void answer3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void answer4_Click(object sender, RoutedEventArgs e)
         {
 
         }
